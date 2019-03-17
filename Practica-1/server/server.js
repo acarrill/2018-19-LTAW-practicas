b5075a3939cf97c7d1b6ac22be2cb34586eb420c
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-03-17 04:41:02 +0100

    Added server and first css and html.

---
+++ Practica-1/server/server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var mime = require('mime-types');
+
+http.createServer((req, resp) => {
+  url = ('.' + req.url);
+  console.log(url);
+  console.log(mime.lookup(url));
+  fs.readFile(url, (err, resource) => {
+    if (err) {
+      resp.writeHead(404);
+      resp.end('Content not found')
+    }else{
+      resp.writeHead(200, {'Content-Type': mime.lookup(url)});
+      resp.end(resource);
+    }
+  })
+}).listen(8080);
