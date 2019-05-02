7bdca7d7db7cbd6bb5b8fc5c65721b5c11510ec9
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-05-02 03:38:19 +0200

    add: changes in style and model

--- Practica-2/mi_proyectoweb/mi_tienda/models.py
+++ Practica-2/mi_proyectoweb/mi_tienda/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    stock = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
--- Practica-2/mi_proyectoweb/mi_tienda/views.py
+++ Practica-2/mi_proyectoweb/mi_tienda/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from mi_tienda.models import Product
 
 # Create your views here.
 
 def home_view (request):
-    return render(request, 'tienda.html', {})
+    p1 = Product.objects.get(name='animal')
+    return render(request, 'tienda.html', {'product':p1.name, 'price':p1.price})
