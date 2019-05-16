de6721ad037fceaa9c2cc9a1919d4677b299a072
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-05-16 03:13:42 +0200

    add: style changes

--- Practica-2/mi_proyectoweb/mi_tienda/views.py
+++ Practica-2/mi_proyectoweb/mi_tienda/views.py
@@ -7,5 +7,6 @@ from mi_tienda.models import Album
 # Create your views here.
 
 def home_view (request):
-    p1 = Album.objects.get(name='WISH YOU WERE HERE')
+    p1 = Album(name='Wish You Were Here')
+    print(p1)
     return render(request, 'tienda.html', {'product':p1.name, 'price':p1.price})
