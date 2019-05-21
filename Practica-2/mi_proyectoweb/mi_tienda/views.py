09a669d2bc440b39f0219da783f3f129e12f0025
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-05-21 18:08:00 +0200

    add: url-views albums almost finished, changes in models and style

--- Practica-2/mi_proyectoweb/mi_tienda/views.py
+++ Practica-2/mi_proyectoweb/mi_tienda/views.py
@@ -11,4 +11,12 @@ def home_view (request):
     return render(request, 'tienda.html', {'albums': albums})
 
 def search_try (request):
+    if request.method == 'GET':
+        search_query = request.GET.get('search-box', None)
+
+
     return render(request, 'search.html', {})
+
+def album_view (request, album_name):
+    
+    return render(request, 'album_page.html', {})
