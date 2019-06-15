d9748b3dfc3d0a8bda8820d11930912caadd8ba1
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-06-16 00:41:42 +0200

    Add: compra html show cart

--- Practica-3/server/static/cart.js
+++ Practica-3/server/static/cart.js
@@ -2,6 +2,7 @@ function test() {
   let cookies = document.cookie;
   let cartMssg = document.getElementsByClassName("cart-mssg");
   let cartContent = document.createElement("P");
+  let cartEmpty = true;
 
   cartContent.innerText = "Your current cart:" + '\n';
   cookies = cookies.split(';');
@@ -10,8 +11,13 @@ function test() {
     cookieName = cookies[cookie].split('=')[0];
     cookieValue = cookies[cookie].split('=')[1];
     if (cookieName.includes('product')) {
+      cartEmpty = false;
       cartContent.innerText += cookieValue + '\n';
       cartMssg[0].appendChild(cartContent);
     }
+  }
+  if (cartEmpty) {
+    console.log("dfjsdfhb");
+    document.getElementsByClassName('error-mssg')[0].style.display = "block";
   }
 }
